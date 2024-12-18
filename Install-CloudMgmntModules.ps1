8306ed3f8605f1e5ac32037d3491483168786a40
cquresphere <72066881+cquresphere@users.noreply.github.com>
2024-12-18 13:13:47 +0100

    Update Install-CloudMgmntModules.ps1
    Improve  error handling and some text details

--- Install-CloudMgmntModules.ps1
+++ Install-CloudMgmntModules.ps1
@@ -9,6 +9,11 @@ $Modules = @(
     "PNP.PowerShell"
 )
 
+$PackageProviderNames = (Get-PackageProvider).Name
+if($PackageProviderNames -notcontains 'NuGet'){
+    Install-PackageProvider -Name NuGet -minimumVersion 2.8.5.201 -Force
+}
+
 Foreach($Module in $Modules){
     $ModuleVer = $null
     $InstalledVer = $null
@@ -31,10 +36,30 @@ Foreach($Module in $Modules){
     }
 
     Try{
-        # Install most recent version of module
+        # Install the most recent version of the module
         Write-Host "Installing $Module version $ModuleVer"
-        Install-Module $module -force -ErrorAction Stop
-    }Catch{
+        Install-Module -Name $module -force -ErrorAction Stop
+    }
+    Catch{
         Write-Host "Error Installing $module `n : $_ `n `n" -ForegroundColor Red
+        $global:Error[0].Exception.GetType().FullName
+        $global:Error[0]
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $CallAPI = Invoke-WebRequest 'https://www.powershellgallery.com/api/v2'
+        if($($CallAPI.StatusCode) -ne 200){
+            Write-Host 'Check your network connection. Confirm that your firewall does not block the powershell.exe and the ieexec.exe' -ForegroundColor Red
+            throw
+        }
+        Set-ExecutionPolicy RemoteSigned -Force
+
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        try{
+            Install-Module $Module -Force -AllowClobber -ErrorAction Stop
+        }
+        catch{
+            $global:Error[0].Exception.GetType().FullName
+            # Install module
+            Install-Module $Module -Force -AllowClobber -ErrorAction Stop -SkipPublisherCheck
+        }
     }
 }
